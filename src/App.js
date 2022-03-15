7260a712849d3ac5cc1d6c5c3e85ad2533004ac1
Gabriel Kasten <gabrieldegkasten@gmail.com>
2022-03-15 08:32:15 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Button from './components/Button.js'
+import "./css/style.css";
 
 class App extends Component {
   
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class Button extends Component {
     render(){
         return(
-            <div>
+            <div className={`collum-${this.props.cols}`}>
                 <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
             </div>
         );
